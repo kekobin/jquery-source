ed334424fa8fca3596f8aecf7629c2a214937e7d
kebin <kekobin@163.com>
2016-07-18 18:20:21 +0800

    add

--- deferred/util.js
+++ deferred/util.js
@@ -6,8 +6,20 @@
 
 	Util.Callbacks = function(options) {
 		var list = [],
-			opts = options && options.match(whitespaceReg) || [],
-			self;
+			opts = options && handleOpts(options) || {},
+			self,
+			memory;
+
+		function handleOpts(opts) {
+			var result = {};
+			opts = opts.match(whitespaceReg);
+
+			opts.forEach(function(key) {
+				result[key] = true;
+			});
+
+			return result;
+		}
 
 		self = {
 			add: function() {
@@ -16,30 +28,43 @@
 						slice.call(funcs).forEach(function(func, i) {
 							if(toString.call(func) == "[object Function]") {
 								list.push(func);
-							} else if(toString.call(func) == "[object Array]" && func.length > 0) {
+							} else if(func && func.length && toString.call(func) != "[object String]") {//这里是数组，也可能是伪数组
 								add(func);
 							}
 						});
 					})(arguments);	
+
+					if(memory) {
+						self.fire(memory);
+					}
 				}
-				
+
 				return this;//done().fail()等链式的关键(哪个对象指向add，返回的就是哪个对象)
 			},
-			fire: function() {
+			fire: function(data) {
+				memory = opts.memory && data;
+
 				if(list && list.length > 0) {
 					(function(self, args) {
 						list.forEach(function(func, i) {
 							func.apply(args[0], args[1]);//argus[0]->context
 						});
 					})(this, arguments);
+
+
 				}
 
-				if(opt[0] == "once") {
-					self.disable();
+				if(list) {
+					if(memory) {
+						list = [];
+					} else {
+						self.disable();
+					}
 				}
 			},
 			disable: function() {
 				list = undefined;
+				return this;
 			}
 		};
 
@@ -48,17 +73,34 @@
 
 	Util.Deferred = function() {
 		var tuples = [
-			["resolve", "done", Util.Callbacks('once')],
-			["reject", "fail", Util.Callbacks('once')],
+			["resolve", "done", Util.Callbacks('once memory')],
+			["reject", "fail", Util.Callbacks('once memory')],
 			["notify", "progress", Util.Callbacks()]
 		],
-		deferred = {};
+		deferred = {},
+		promise = {
+			promise: function() {
+				return promise;
+			},
+			always: function() {
+				deferred.done(arguments).fail(arguments);
+
+				return this;
+			},
+			then: function() {
+				deferred.done(arguments[0]).fail(arguments[1]).progress(arguments[2]);
+
+				return this;
+			}
+		};
 
 		tuples.forEach(function(tuple, i) {
 			var cb = tuple[2];
 
-			deferred[ tuple[1] ] = cb.add;
+			//promise['done' | 'fail' | 'progress']
+			promise[ tuple[1] ] = cb.add;
 
+			//deferred['resolve' | 'reject' | 'notify']
 			deferred[ tuple[0] ] = function() {
 				deferred[ tuple[0] + 'With'](this, arguments);
 
@@ -68,8 +110,47 @@
 			deferred[ tuple[0] + 'With'] = cb.fire;
 		});
 
+		extend(deferred, promise);
+
 		return deferred;
 	};
+
+	Util.when = function() {
+		var resolveValues = slice.call(arguments),
+			length = resolveValues.length, deferred,
+			resolveContexts,
+			remaining = length != 1 ? length : 0;
+
+		deferred = length == 1 ? resolveValues[0] : Util.Deferred();
+
+		if(!length) {
+			deferred.resolve(resolveContexts, resolveValues);
+		}
+
+		return deferred.promise();
+	};
+
+	function extend() {
+		var target = arguments[0] || {},
+			slice = Array.prototype.slice,
+			options;
+
+		if(!arguments) return;
+
+		options = slice.call(arguments).slice(1);
+
+		for(var i = 0, len = options.length; i < len; i++) {
+			var obj = options[i];
+
+			for(var key in obj) {
+				var value = obj[key];
+				target[key] = value;
+			}	
+		}
+		
+
+		return target;
+	}
 
 	window.Util = Util;
 })(window);
