a2d5b654ec1d8e6c3f9d1dc095e7c8f6da09f4f1
Jimmy <root@myWIFI.local>
2016-07-18 08:30:59 +0800

    add

--- deferred/util.js
+++ deferred/util.js
@@ -1,41 +1,55 @@
 (function() {
 	var Util = {},
 		slice = Array.prototype.slice,
-		toString = Object.prototype.toString;
+		toString = Object.prototype.toString,
+		whitespaceReg = /\S+/g;
 
-	Util.Callbacks = function() {
-		var list = [];
+	Util.Callbacks = function(options) {
+		var list = [],
+			opts = options && options.match(whitespaceReg) || [],
+			self;
 
-		return {
+		self = {
 			add: function() {
-				(function add(funcs) {
-					slice.call(funcs).forEach(function(func, i) {
-						if(toString.call(func) == "[object Function]") {
-							list.push(func);
-						} else if(toString.call(func) == "[object Array]" && func.length > 0) {
-							add(func);
-						}
-					});
-				})(arguments);
-
+				if(list) {
+					(function add(funcs) {
+						slice.call(funcs).forEach(function(func, i) {
+							if(toString.call(func) == "[object Function]") {
+								list.push(func);
+							} else if(toString.call(func) == "[object Array]" && func.length > 0) {
+								add(func);
+							}
+						});
+					})(arguments);	
+				}
+				
 				return this;//done().fail()等链式的关键(哪个对象指向add，返回的就是哪个对象)
 			},
 			fire: function() {
 				if(list && list.length > 0) {
 					(function(self, args) {
 						list.forEach(function(func, i) {
-							func.call(self, args[0]);
+							func.apply(args[0], args[1]);//argus[0]->context
 						});
 					})(this, arguments);
 				}
+
+				if(opt[0] == "once") {
+					self.disable();
+				}
+			},
+			disable: function() {
+				list = undefined;
 			}
-		}
+		};
+
+		return self;
 	}
 
 	Util.Deferred = function() {
 		var tuples = [
-			["resolve", "done", Util.Callbacks()],
-			["reject", "fail", Util.Callbacks()],
+			["resolve", "done", Util.Callbacks('once')],
+			["reject", "fail", Util.Callbacks('once')],
 			["notify", "progress", Util.Callbacks()]
 		],
 		deferred = {};
